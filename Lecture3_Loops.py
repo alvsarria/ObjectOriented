dd6474e6aad42c7d7f7cc93abe2c0ecbde80a321
alvsarria <alvaro.sarria.rico>
2019-10-21 22:22:25 +0100

    Committing third lecture exercises - only the statements

---
+++ Lecture3_Loops.py
@@ -0,0 +1,34 @@
+# Exercise 1:
+# Write a WHILEloop that will print the numbers from 1 to 10 on the screen.
+
+# Exercise 2:
+# Write a FOR loop that will print the numbers from 1 to 10 on the screen.
+
+# Exercise 3:
+# Write a FOR loop that will iterate from 0 to 20. For each iteration, it will check if the current number
+# is even or odd, and report that to the screen (e.g. "1 is odd, 2 is even").
+
+# Exercise 4:
+# Write a FOR loop that will iterate from 0 to 10. For each iteration of the loop, it will
+# multiply the number by 9 and printthe result (e.g. "2 * 9 = 18").
+# Bonus (complete at the end of the class once you are comfortable with loops):Use a nested loop to show
+# the tables for every multiplier from 1 to 10 (100 results total).
+
+# Exercise 5:
+# Write a program that asks the user for a number and printsthe sum of all numbers from 1 to the number they enter.
+
+# Exercise 6:
+# Write a program to calculate and print the factorial of a number using a FORloop. The factorial of a number is
+# the product of all integers up to and including that number, so the factorial of 4 is 4*3*2*1= 24
+
+# Exercise 7:
+# Ask the user to enter a number andprint it back on the screen. Keep asking for a new number
+# until they enter a negative number.
+
+# Exercise 8:
+# Write a program that uses loops to print the triangle below
+# Hint 1: you will need to use nested loops.
+# Hint 2: on line 1 we print 1 *, on line 2 we print 2 stars... on line x we print x stars...)
+
+# Exercise 9:
+# Write a program that uses loops to print the triangle below (hint: you will need to use nested loop)
