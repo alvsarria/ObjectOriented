00064a6f7de823185a064c591cb3e640e4e55768
alvsarria <alvaro.sarria.rico>
2019-11-22 21:02:08 +0000

    Committing third lecture exercises - only the statements

---
+++ Lecture7_Lists.py
@@ -0,0 +1,11 @@
+# Exercise 1:Write a Python function to sum all numbers in a list.
+#
+# Exercise 2:Write a Python function to get the largest number from a list.
+#
+# Exercise 3: Write a Python function that takes a list of words and counts how many of them begin with ‘a’.
+#
+# Exercise 4: (modify Ex3) Write a Python function that takes a list of words and a character, and counts
+# how many of the words in the list begin with that character.
+#
+# Exercise 5:Write a Python function that takes a list of numbers and returns a new list containing
+# only the even numbers from the first list.
