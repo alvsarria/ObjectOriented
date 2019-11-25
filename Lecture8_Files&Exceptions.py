fca9137a40eac9c1d0ae06832a40a3068e210c52
alvsarria <alvaro.sarria.rico>
2019-11-25 19:53:04 +0000

    Final version of the exercises

--- Lecture8_Files&Exceptions.py
+++ Lecture8_Files&Exceptions.py
@@ -1,25 +1,79 @@
 file_path = "C://Users//alvar//Documents//DT265C - Fundamentals of Computing//Object Oriented SW - GitHub//" \
             "Lecture8_Files&Exceptions//"
 
-file_obj = open(file_path+"twinkle.txt", "r+")
+# Exercise 1: Write a Python program that opens a text file, reads the contents line by line and saves it
+# into a second file. Make sure you use try/except to handle and possible problems with incorrect filenames.
 
-for line in file_obj:
-    print(line.strip())
+def open_ex1():
+    try:
+        file_obj = open(file_path + "twinkle.txt", "r")
+        file_obj_2 = open(file_path + "twinkle_ex1.txt", "w")
+    except FileNotFoundError:
+        print("The file you are trying to read does not exist")
+    else:
+        for line in file_obj:
+            print(line.strip())
+            # print(line.strip(), file=file_obj_2)
+            file_obj_2.write(line.strip() + "\n")
 
-file_obj.close()
+        print("Second file created correctly")
+        file_obj.close()
+        file_obj_2.close()
 
-output_file = open("numbers.txt","w")
+# open_ex1()
 
-for i in range(0,11):
-    output_file.write(str(i))
-    output_file.write("\n")
+# Exercise 2: Write a Python program that opens a text file, reads the contents line
+# by line and prints each line in reverse.
 
-output_file.close()
+def open_ex2():
+    try:
+        file_obj = open(file_path + "twinkle.txt", "r")
+        file_obj_2 = open(file_path + "twinkle_ex2.txt", "w")
+    except FileNotFoundError:
+        print("The file you are trying to read does not exist")
+    else:
+        for line in file_obj:
+            my_line = list(line.strip())
+            my_line.reverse()
+            # print("".join(my_line), file=file_obj_2)
+            file_obj_2.write("".join(my_line) + "\n")
 
-number = input("Enter a number")
-try:
-    print("Number multiplied by two is",int(number)*2)
-except ValueError:
-    print("Not a number, cannot be multiplied")
-else:
-    print("Multiplication successful!")
+        print("Second file created correctly")
+        file_obj.close()
+        file_obj_2.close()
+
+# open_ex2()
+
+# Exercise 3: Write a Python program that reads a text file and prints only the lines that start with 'When'.
+
+def open_ex3():
+    try:
+        file_obj = open(file_path + "twinkle.txt", "r")
+    except FileNotFoundError:
+        print("The file you are trying to read does not exist")
+    else:
+        for line in file_obj:
+            if line.strip()[0:4] == "When":
+                print(line.strip())
+
+        file_obj.close()
+
+# open_ex3()
+
+# Exercise 4: Write a Python program that reads a text file and prints the length of each line.
+
+def open_ex4():
+    try:
+        file_obj = open(file_path + "twinkle.txt", "r")
+    except FileNotFoundError:
+        print("The file you are trying to read does not exist")
+    else:
+        for line in file_obj:
+            # Count by characters
+            print(len(list(line.strip())))
+            # Count by words
+            # print(len(line.strip().split(" ")))
+
+        file_obj.close()
+
+# open_ex4()
