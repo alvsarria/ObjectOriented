07519a81adf39fd147a97706b03b475635a438ef
Alvaro Sarria Rico <alsarria@fb.com>
2019-11-26 22:54:06 +0000

    Third exercise

--- Lecture9_Revision.py
+++ Lecture9_Revision.py
@@ -52,7 +52,27 @@ else:
 # a list of numbers to be replaced and a list of numbers to use as replacements.For example
 # replace_all([1,2,5,6,2,7,1,2], [2,4],[200,400]) will replace all occurrences of 2 with 200 and all occurrences of
 # 4 with 400 in the list [1,2,5,6,2,7,1,2], so should return [1,200,5,6,200,7,1,200]
-#
+
+def replace_all(list_init, l_out, l_in):
+    # copy method to create an independent object which does not point to the value of the object copied (in memory)
+    # list_to_check = list_init.copy()
+    if len(l_out) != len(l_in):
+        print("l_out needs to have the same length as l_in")
+    else:
+        list_final = []
+        for i in range(0,len(list_init)):
+            list_final.append(list_init[i])
+            for j in range(0,len(l_out)):
+                if l_out[j] == list_init[i]:
+                    list_init[i] = l_in[j]
+        if list_init == list_final:
+            print("There is no ocurrence therefore the output list is the same:")
+            print(list_init)
+        else:
+            print(list_init)
+
+replace_all([1,2,5,6,2,7,1,2],[2,4],[200,400])
+
 # Exercise 4: Write a Python function to replace every third word in a sentence with “hello”. Use the function to read
 # a text from a file, replace every third word with ‘hello’ and write the output in another file.
 #
