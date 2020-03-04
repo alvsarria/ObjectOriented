39c10369333eb70e04de351901af6c24c81a6cbf
Alvaro Sarria Rico <alsarria@fb.com>
2020-03-04 23:03:56 +0000

    - Start designing search function

--- CA_Library/library.py
+++ CA_Library/library.py
@@ -5,8 +5,45 @@ from add_book import add
 
 inventory = upload()
 
-status(inventory)
+def cleaner(input):
+    for i in [",", ".", ";", ":", "!", "?","'s","s'"]:
+        input = input.replace(i,"")
+    input = (input.lower()).split(" ")
+    output = []
+    for i in input:
+        if i not in ["a","of","the","and"]:
+            output.append(i)
+    output = " ".join(output)
+    return output
 
-inventory = add(inventory)
+
+def search(key_word,inventory):
+    titles = []
+    titles_real = []
+    key_word = cleaner(key_word)
+    search_keywords = key_word.split(" ")
+    for key in inventory:
+        titles.append(cleaner(inventory[key][0]))
+        titles_real.append(inventory[key][0])
+    scores = []
+    for i in titles:
+        count = 0
+        title_keywoards = i.split(" ")
+        for j in title_keywoards:
+            for n in search_keywords:
+                if j == n:
+                    count += 1
+        scores.append(count)
+    shell_scores = []
+    for i in range(len(titles_real)):
+        shell_scores.append([scores[i],titles_real[i]])
+    shell_scores.sort(reverse = True)
+    count = 0
+    for i in shell_scores:
+        if i[0] != 0:
+            count += 1
+            print(str(count) + ' || ' + i[1])
+
+search("harry potter",inventory)
 
 
