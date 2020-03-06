e6c99caa1c2d67aa9673e8bb87eb98bd439ba82f
Alvaro Sarria Rico <alsarria@fb.com>
2020-03-06 16:54:25 +0000

    - some changes

--- CA_Library/download_functions.py
+++ CA_Library/download_functions.py
@@ -1,13 +1,13 @@
 # function to bulk the inventory into our .txt database
 def download_inventory(inventory):
-    file_obj = open("DBs/library_inventory.txt", "w")
+    file_obj = open("dbs/library_inventory.txt", "w")
     for i in inventory:
         element = [i, inventory[i][0], inventory[i][1], inventory[i][2], inventory[i][3]]
         file_obj.write(";;".join(element) + "\n")
     file_obj.close()
 
 def download_log_file(user_log_file):
-    file_obj = open("DBs/checkout_books_log_file.txt", "w")
+    file_obj = open("dbs/checkout_books_log_file.txt", "w")
     for i in user_log_file:
         element = i.strip()
         file_obj.write(element + "\n")
--- CA_Library/library.py
+++ CA_Library/library.py
@@ -1,8 +1,8 @@
-from status_functions import *
-from add_book import add
-from search_book import search
-from checkout_book import checkout
-from return_book import return_book
+from functions.status_functions import *
+from functions.add_book import add
+from functions.search_book import search
+from functions.checkout_book import checkout
+from functions.return_book import return_book
 
 status_inventory()
 status_log_file()
